7ad05a934671af0f1cd98f38ef84756036f64982
claytantor <claytantor@gmail.com>
2019-12-07 17:23:13 -0800

    added baseline monitoring

---
+++ README.md
@@ -0,0 +1,11 @@
+# redbluff-dam-alerts
+send dam diversion alerts to the slack channel
+
+## installing stuff
+```
+python3 -m venv ./venv
+source venv/bin/activate
+$(pwd)/venv/bin/python3 -m pip install --upgrade pip
+$(pwd)/venv/bin/python3 -m pip install -r requirements.txt
+```
+
---
+++ app.py
@@ -0,0 +1,139 @@
+import logging
+import argparse
+import yaml
+import sys, os
+import feedparser
+import requests
+import json
+import re
+
+from lxml import html
+from os.path import dirname, abspath
+
+LOG_FORMAT = ('%(levelname) -10s %(asctime)s %(funcName) '
+              '-35s %(lineno) -5d: %(message)s')
+
+LOGGER = logging.getLogger(__name__)
+
+class SlackWebhookNotification:
+    def __init__(self, source, notification, lines):
+        self.source = source
+        self.lines = lines
+        self.notification = notification
+
+    @staticmethod    
+    def matches(notification):
+        return (re.search("^SlackWebhook", notification['type']) != None)
+    
+    def notify(self):
+        """
+        # curl -X POST -H 'Content-type: application/json' --data '{"text":"Hello, World!"}' https://hooks.slack.com/services/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
+        """
+
+        self.notification['content'].extend(self.lines)
+        #notityLines = self.notification['content'].extend(self.lines) 
+        #print('\n'.join(self.notification['content']))
+        
+        response = requests.post(self.notification['path'], data=json.dumps({'mrkdwn': True, 'text':'\n'.join(self.notification['content'])}), headers={'Content-type':'application/json'})
+        print('NOTIFY SlackWebhook!', self.notification['path'], response)
+
+
+class FlashLexNotification:
+    def __init__(self, source, notification, lines):
+        self.source = source
+        self.lines = lines
+        self.notification = notification
+    
+    @staticmethod    
+    def matches(notification):
+        return (re.search("^FlashLex", notification['type']) != None)
+
+    @classmethod
+    def notify(self):
+        pass
+
+
+def loadConfig(configFile):
+    cfg = None
+    with open(configFile, 'r') as ymlfile:
+        cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+    return cfg
+
+def getForecastLines(elementString):
+    tree = html.fromstring(elementString.replace('\n',''))
+    forcasts = tree.xpath('//div/text()')
+    return forcasts
+
+def linesIsTriggered(source, lines):
+    print(source['trigger'])
+    #filter the criteria
+    for line in lines:
+        x = re.search("^{0}".format(source['trigger']['match']), line)
+        if(x):
+            #print(line, x)
+            parts = line.split()
+            
+            if(len(parts)==6):
+                height = float(parts[4])
+                if height>=source['trigger']['minor']:
+                    print('ALERTING level above minimum threshold:{0}'.format(source['trigger']['minor']))
+                    return True
+
+                else:
+                    print('Forcasted level {0} ft is below minor threshold {1} ft'.format(height, source['trigger']['minor']))
+    return False
+
+def makeNotifier(notification, source, lines):
+    if(SlackWebhookNotification.matches(notification)):
+        return SlackWebhookNotification(source, notification, lines)
+    else:
+        return None
+
+def notifySource(source, lines):
+    for notification in source['notifications']:
+        notifier = makeNotifier(notification, source, lines)
+        if(makeNotifier != None):
+            notifier.notify()
+        
+
+def parseSource(source, config):
+    
+    d = None
+    if(source['source-type'] == 'file'):
+        d = feedparser.parse(source['path'])
+
+    if(source['source-type'] == 'url'):
+        d = feedparser.parse(source['path'])    
+    
+    if(d != None):
+        for entry in d['entries']:
+            forecastLines = getForecastLines(entry['summary'])
+            if linesIsTriggered(source, forecastLines):
+                notifySource(source, forecastLines)
+
+                #print(forecastLines)
+
+def main(argv):
+    print("starting app.")
+
+    # get the default paths for both keys and data
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    parent_dir = dirname(dirname(abspath(__file__)))
+
+    # Read in command-line parameters
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-c", "--config", action="store", 
+        required=True, dest="config", help="the YAML configuration file")
+
+    args = parser.parse_args()
+    config = loadConfig(args.config)['noaa-alerts']
+
+    for source in config['sources']:
+        parseSource(source, config)
+
+
+    
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
